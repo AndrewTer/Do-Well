9fce915306f3d175bf7180e9d87248e8b2e34485
AndrewCTer <33009070+AndrewTer@users.noreply.github.com>
2019-12-04 17:26:00 +0300

    Add files via upload

---
+++ form.php
@@ -0,0 +1,18 @@
+<?php
+echo "
+	<html>
+		<head>
+			<script src='jquery-1.7.1.min.js'></script>
+			<script src='ajax.js'></script>
+		</head>
+		<body>    
+			<form action='' id='ajax_form'  method='POST'>
+				<p>Name: <input type='input' name='name' maxlength='50' required /></p>
+				<p>Email: <input type='email' name='email' maxlength='50' required /></p>
+				<p><input type='button' id='send' value='Отправить заявку на обратный звонок' /></p>
+			</form> 
+			<div id='result_answer'></div>  
+		</body>
+	</html>
+	"
+?>
