32da7076b7b52b7a99e74331b27c8df8ef34c67a
AndrewCTer <33009070+AndrewTer@users.noreply.github.com>
2019-12-04 18:45:32 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<html>
+	<head>
+		<link href="styles.css" media="screen" rel="stylesheet"/>
+	</head>
+	<body>
+		<div class="wrapper1">
+		   <div>1</div>
+		   <div>2</div>
+		   <div>3</div>
+		   <div>4</div>
+		   <div>5</div>
+		   <div>6</div>
+		   <div>7</div>
+		   <div>8</div>
+		   <div>9</div>
+		   <div>10</div>
+		   <div>11</div>
+		   <div>12</div>
+		</div>
+
+		<div class="wrapper2">
+		   <div>1</div>
+		   <div>2</div>
+		   <div>3</div>
+		   <div>4</div>
+		   <div>5</div>
+		   <div>6</div> 
+		   <div>7</div>
+		   <div>8</div>
+		   <div>9</div>
+		   <div>10</div>
+		   <div>11</div>
+		   <div>12</div>
+		</div>
+
+		<div class="wrapper3">
+		   <div>1</div>
+		   <div>2</div>
+		   <div>3</div>
+		   <div>4</div>
+		   <div>5</div>
+		   <div>6</div>
+		   <div>7</div>
+		   <div>8</div>
+		   <div>9</div>
+		   <div>10</div>
+		   <div>11</div>
+		   <div>12</div>
+		</div>
+	</body>
+</html>
