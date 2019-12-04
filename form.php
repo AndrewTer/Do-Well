c457822a1e18d553416e9f609f8b583a40279f75
AndrewCTer <33009070+AndrewTer@users.noreply.github.com>
2019-12-04 17:21:09 +0300

    Add files via upload

---
+++ form.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Article FRUCTCODE.COM. How to send html-form with Ajax.</title>
+		
+		<script src="jquery-1.7.1.min.js"></script>
+		<script src="ajax.js"></script>
+	</head>
+	<body>    
+		<form action="" id="ajax_form"  method="POST">
+			<p>Name: <input type="input" name="name" maxlength="50" required /></p>
+			<p>Email: <input type="email" name="email" maxlength="50" required /></p>
+			<p><input type="button" id="send" value="Отправить заявку на обратный звонок" /></p>
+		</form> 
+		<div id="result_answer">x</div>  
+	</body>
+</html>
